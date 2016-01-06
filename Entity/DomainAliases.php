7961bc72fc082b8ae8becd9ed52aba76aa10411c
Can Berkol <can.berkol@biberltd.com>
2016-01-06 18:38:51 +0200

    =innodb directives removed from annotations

--- Entity/DomainAliases.php
+++ Entity/DomainAliases.php
@@ -23,8 +23,7 @@ use BiberLtd\Bundle\CoreBundle\CoreEntity;
  * @ORM\Entity
  * @ORM\Table(
  *     name="domain_aliases",
- *     schema="innodb",
- *     options={"charset":"utf8","collate":"utf8_turkish_ci","engine":"innodb"}
+ *     options={"charset":"utf8","collate":"utf8_turkish_ci"}
  * )
  */
 class DomainAliases extends CoreEntity
