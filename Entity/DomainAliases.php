f1426c426bbc3c1539fa71d32723643313d66496
Said İmamoğlu <said.imamoglu@biberltd.com>
2016-01-20 12:48:28 +0200

    Domain aliases entity updated.

--- Entity/DomainAliases.php
+++ Entity/DomainAliases.php
@@ -24,19 +24,19 @@ use BiberLtd\Bundle\CoreBundle\CoreEntity;
  * @ORM\Table(
  *     name="domain_aliases",
  *     schema="innodb",
- *     options={"charset":"utf8","collate":"utf8_turkish_ci","engine":"innodb"}
+ *     options={"charset":"utf8","collate":"utf8_turkish_ci"}
  * )
  */
 class DomainAliases extends CoreEntity
 {
     /**
-	 * 
+	 * @ORM\Id
      * @ORM\Column(type="text", unique=true, nullable=true, options={"default":"Alias - domain. i.e. biberltd.com"})
      */
     private $domain;
 
     /**
-	 * 
+	 * @ORM\Id
      * @ORM\ManyToOne(targetEntity="BiberLtd\Bundle\SiteManagementBundle\Entity\Site", inversedBy="domains")
      * @ORM\JoinColumn(name="site", referencedColumnName="id", onDelete="CASCADE")
      */
