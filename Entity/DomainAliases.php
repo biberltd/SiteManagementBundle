b48edb029572bfb3defc05eeeaa6964c1fdbc079
Can Berkol <can.berkol@biberltd.com>
2016-02-19 16:02:07 +0200

    orm fixed

--- Entity/DomainAliases.php
+++ Entity/DomainAliases.php
@@ -22,8 +22,7 @@ use BiberLtd\Bundle\CoreBundle\CoreEntity;
 /**
  * @ORM\Entity
  * @ORM\Table(
- *     name="domain_aliases",
- *     schema="innodb",
+ *     name="domain_aliases"
  *     options={"charset":"utf8","collate":"utf8_turkish_ci","engine":"innodb"}
  * )
  */
